ae49f62e0a879da94f5055d69cccb5dfe7088da1
Louis Taylor <kragniz@gmail.com>
2013-04-16 21:30:17 +0100

    Added setup script

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='wch',
+    version='0.1',
+    description='Small mplayer wrapper which allows restarting from the episode last watched',
+    author='Louis Taylor',
+    author_email='kragniz@gmail.com',
+    url='https://github.com/kragniz/wch',
+    scripts=['wch', 'wch_log']
+)
